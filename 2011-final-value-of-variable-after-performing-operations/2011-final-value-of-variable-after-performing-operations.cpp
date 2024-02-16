33c2ece3241812db63729a2efce71548720cc00c
Aryan N. Jain <aryanj1084@gmail.com>
2024-02-16 22:23:42 +0530

    Time: 6 ms (66.82%), Space: 17.6 MB (39.24%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int X = 0;
-        int n = operations.size();
-        for (int i = 0; i < n; i++) {
+        //int n = operations.size();
+        for (int i = 0; i < operations.size(); i++) {
             if (operations[i] == "--X" || operations[i] == "X--") {
                 X -= 1;
             } else {
-                X = X + 1;
+                X+=1;
             }
         }
         return X;
