6758b3deb74eef3784c7b9e3984d45af59087064
Aryan N. Jain <aryanj1084@gmail.com>
2024-03-05 16:03:27 +0530

    Time: 27 ms (45.86%), Space: 13.8 MB (70.34%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int l=0;
+        int r=s.length()-1;
+        while(l<r && s[l]==s[r])
+        {
+            char current=s[l];
+            while(l<=r && s[l]==current)
+            l++;
+            while(r>=l && s[r]==current)
+            r--;
+        }
+        return r-l+1;
+    }
+};
